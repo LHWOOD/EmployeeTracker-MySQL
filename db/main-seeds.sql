cb8b5c035e99d469885d84830a0020bf501a5fe7
LHWOOD <hutsonwood@gmail.com>
2021-05-23 16:43:02 -0600

    updated the schema in the seeds.sql file

--- db/main-seeds.sql
+++ db/main-seeds.sql
@@ -1,4 +1,4 @@
-ROP DATABASE IF EXISTS tracker_DB;
+DROP DATABASE IF EXISTS tracker_DB;
 
 CREATE DATABASE tracker_DB;
 
@@ -13,7 +13,8 @@ CREATE TABLE role (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL(8,2),
-  department_id INT
+  department_id INT,
+  FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -21,7 +22,9 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
-  manager_id INT
+  manager_id INT,
+  FOREIGN KEY(role_id) REFERENCES role(id),
+  FOREIGN KEY(manager_id) REFERENCES employee(id)
 );
 
 
