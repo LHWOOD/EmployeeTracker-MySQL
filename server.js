de8eba9aa18ea655204e1dd7582fc24ef358a577
LHWOOD <hutsonwood@gmail.com>
2021-05-23 17:41:59 -0600

    created the start() in server.js and created the first inquirer question

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const chalk = require("chalk");
 const cTable = require("console.table");
+const mainScreen = [
+  "View All Employees",
+  "View All Employees By Department",
+  "View All Employees by Manager",
+];
 
 //connection object
 const connection = mysql.createConnection({
@@ -13,15 +18,17 @@ const connection = mysql.createConnection({
   database: "tracker_db",
 });
 
-const test = () => {
-  connection.query("SELECT * FROM department", (err, res) => {
-    if (err) throw err;
-    console.table(res);
+const start = () => {
+  inquirer.prompt({
+    type: "list",
+    name: "main",
+    message: "What would you like to do?",
+    choices: mainScreen,
   });
 };
 
 connection.connect((err) => {
   if (err) throw err;
   console.log(chalk.blue(`connected as id ${connection.threadId}\n`));
-  test();
+  start();
 });
