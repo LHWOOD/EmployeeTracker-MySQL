9706c958e38248fe4ad9ba9f866965b3a8e8dcc7
LHWOOD <hutsonwood@gmail.com>
2021-05-23 20:57:47 -0600

    created the start() with a switch of all the main options

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const mainScreen = [
   "View All Employees",
   "View All Employees By Department",
   "View All Employees by Manager",
+  "Add Employees",
+  "Remove Employees",
+  "Update Employee Role",
+  "Update Employee Manager",
+  "View All Roles",
+  "Add Role",
+  "Remove Role",
 ];
 
 //connection object
@@ -19,12 +26,47 @@ const connection = mysql.createConnection({
 });
 
 const start = () => {
-  inquirer.prompt({
-    type: "list",
-    name: "main",
-    message: "What would you like to do?",
-    choices: mainScreen,
-  });
+  inquirer
+    .prompt({
+      type: "list",
+      name: "main",
+      message: "What would you like to do?",
+      choices: mainScreen,
+    })
+    .then((answer) => {
+      switch (answer.main) {
+        case "View All Employees":
+          viewAll();
+          break;
+        case "View All Employees by Department":
+          viewByDept();
+          break;
+        case "View All Employees by Manager":
+          viewByManager();
+          break;
+        case "Add Employees":
+          addEmployees();
+          break;
+        case "Remove Employees":
+          removeEmployees();
+          break;
+        case "Update Employee Role":
+          updateRole();
+          break;
+        case "Update Employee Manager":
+          updateManager();
+          break;
+        case "View All Roles":
+          viewAllRoles();
+          break;
+        case "Add Role":
+          addRoles();
+          break;
+        case "Remove Role":
+          removeRole();
+          break;
+      }
+    });
 };
 
 connection.connect((err) => {
